b90688c66d44d27336f59f769d0c0b03da27d7e5
Leonardo <lporchat06@gmail.com>
2022-02-23 08:07:41 -0300

    Começo da aplicação

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+
+      }
+    ]
+  }
+
+}
