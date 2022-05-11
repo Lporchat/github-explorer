024f79876154e846a84ffda4f64e18270b94fdca
Leonardo <lporchat06@gmail.com>
2022-05-11 13:01:35 -0300

    inicialndo o babel e o webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,28 @@
-const path = require('path');
-
-
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  mode: 'development',
+  entry: path.resolve(__dirname, "src", "index.jsx"),
   output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-
+  // aquivos suportados
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
-
-      }
-    ]
-  }
-
-}
+        use: "babel-loader",
+      },
+    ],
+  },
+};
